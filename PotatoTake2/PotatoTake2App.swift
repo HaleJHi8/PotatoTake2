cfc77cabbf014e5f302d70720a75674a2439a974
Hale, J <141489@swcsd.us>
2025-05-09 08:18:36 -0400

    Initial Commit

---
+++ PotatoTake2/PotatoTake2App.swift
@@ -0,0 +1,17 @@
+//
+//  PotatoTake2App.swift
+//  PotatoTake2
+//
+//  Created by Jacen H. on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PotatoTake2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
