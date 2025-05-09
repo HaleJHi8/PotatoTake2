f36787fe01548ab669f89b91da55b7a533f08c5e
Hale, J <141489@swcsd.us>
2025-05-09 08:24:27 -0400

    Made small change as test

--- PotatoTake2/ContentView.swift
+++ PotatoTake2/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("J.H. - French fries")
+            Text("Something that doesn't crash it")
         }
         .padding()
     }
